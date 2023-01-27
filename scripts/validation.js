96410c86cc4a431f2dd43ae2abbfe885cc8500d3
Pedrohme <pedrohmen45@gmail.com>
2023-01-26 23:16:28 -0300

    formatação do código com o prettier e ajuste de margem no formulário

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,21 +1,21 @@
 $('#submit').on('click', (e) => {
-  e.preventDefault();
-  e.stopImmediatePropagation();
-  validateRegister();
+	e.preventDefault();
+	e.stopImmediatePropagation();
+	validateRegister();
 });
 
 function validateRegister() {
-  const register = document.forms["register"]
+	const register = document.forms['register'];
 
-  const title = register["title"].value
-  const author = register["author"].value
-  const publisher = register["publisher"].value
-  const year = register["year"].value
-  const name = register["name"].value
-  const email = register["email"].value
+	const title = register['title'].value;
+	const author = register['author'].value;
+	const publisher = register['publisher'].value;
+	const year = register['year'].value;
+	const name = register['name'].value;
+	const email = register['email'].value;
 
-  if (title === "" || author === "" || publisher === "" || year === "" || name === ""|| email === "") {
-    alert("Todos os campos precisam ser preenchidos.");
-    return false;
-  }
+	if (title === '' || author === '' || publisher === '' || year === '' || name === '' || email === '') {
+		alert('Todos os campos precisam ser preenchidos.');
+		return false;
+	}
 }
