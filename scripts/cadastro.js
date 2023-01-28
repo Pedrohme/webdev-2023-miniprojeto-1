3a751174b20d4c96fe5c15445cc69c0296ba1bd7
Pedrohme <pedrohmen45@gmail.com>
2023-01-28 00:06:53 -0300

    melhorias na tela de cadastro

---
+++ scripts/cadastro.js
@@ -0,0 +1,34 @@
+$(() => {
+	$('#submit').on('click', (e) => {
+		// e.preventDefault();
+		// e.stopImmediatePropagation();
+	});
+
+	// Campo de telefone: formata a string automaticamente
+	$('#tel').on('input', function (e) {
+		e.preventDefault();
+
+		const numbers = this.value.replace(/\D/g, '');
+
+		let newVal = '';
+
+		for (let i = 0; i < numbers.length; i++) {
+			switch (i) {
+				case 0:
+					newVal = '(' + numbers[i];
+					break;
+				case 2:
+					newVal += ') ' + numbers[i];
+					break;
+				case 7:
+					newVal += '-' + numbers[i];
+					break;
+				default:
+					newVal += numbers[i];
+					break;
+			}
+		}
+
+		this.value = newVal;
+	});
+});
